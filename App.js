90e41946240ab207d32d1bfcc0cabec99eb674fd
skskdmsdl <skskdmsdl@gmail.com>
2023-01-18 23:24:45 +0900

    work input과 travel input 구분값과 input value object에 추가(object 합치기)

--- App.js
+++ App.js
@@ -1,14 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from "react";
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput} from 'react-native';
 
 import { theme } from "./colors";
 
 export default function App() {
   const [working, setWorking] = useState(true);
+  const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
 
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+  const addToDo = async () => {
+    if (text === "") {
+      return;
+    }
+    // Object.assing 으로 Object 합치기
+    // const newToDos = Object.assign({}, toDos, {
+    //   [Date.now()]: { text, working },
+    // });
+    // ES6 Object 합치기
+    const newToDos = {
+      ...toDos,
+      [Date.now()]: { text, working },
+    };
+    setToDos(newToDos);
+    setText("");
+  };
 
   return (
     <View style={styles.container}>
@@ -31,6 +50,16 @@ export default function App() {
           </Text>
         </TouchableOpacity>
       </View>
+      <TextInput
+          onSubmitEditing={addToDo}
+          onChangeText={onChangeText}
+          returnKeyType="done"
+          value={text}
+          placeholder={
+            working ? "What do you have to do?" : "Where do you want to go?"
+          }
+          style={styles.input}
+        />
     </View>
   );
 }
@@ -49,5 +78,13 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 38,
     fontWeight: "600",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginVertical: 20,
+    fontSize: 18,
   },
 });
